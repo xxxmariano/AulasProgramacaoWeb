6c56206f7ea557e800c1b31ad39413f961a991e9
xxxmariano <47834406+xxxmariano@users.noreply.github.com>
2019-05-31 21:55:04 -0300

    excluir e lista - croud 31/05
    excluir e lista - croud 31/05

--- sistema/crud/controller/usuario/usuario.php
+++ sistema/crud/controller/usuario/usuario.php
@@ -8,11 +8,12 @@
 </head>
 
 <body>
-<?php
+   <?php
    require_once '../../view/nav.php';
    ?>
-<div class="container">
-<div class="row justify-content-center bg-light">
+
+   <div class="container">
+       <div class="row justify-content-center bg-light">
            <form action="usuarioDAO.php" method="POST">
                <div class="form-group">
                    <label>Usuário</label>
@@ -25,11 +26,53 @@
 
                <div class="form-group">
                    <button type="submit" class="btn btn-primary" name="salvar">Salvar</button>
-               
                </div>
            </form>
        </div>
-</div>
+
+       <?php
+       require_once('../banco/conexao.php');
+
+       $resultado = $conexao->query("SELECT * FROM usuario") or die(mysqli_error($conexao));
+       ?>
+
+       <div class="row justify-content-center">
+           <table class="table">
+               <thead>
+                   <tr>
+                       <th>ID</th>
+                       <th>Usuário</th>
+                       <th>Senha</th>
+                       <th colspan="2">Ações</th>
+                   </tr>
+               </thead>
+               <?php
+               while ($row = $resultado->fetch_assoc()) : ?>
+                   <tr>
+                       <td><?php echo $row['id']; ?></td>
+                       <td><?php echo $row['usuario']; ?></td>
+                       <td><?php echo $row['senha']; ?></td>
+                       <td>
+                           <a href="#" class="btn btn-info">Editar</a>
+                           <a href="usuarioDAO.php?excluir=<?php echo $row['id']; ?> " class="btn btn-danger">Excluir</a>
+                       </td>
+                   </tr>
+               <?php endwhile; ?>
+
+           </table>
+       </div>
+
+
+
+
+   </div>
+
 </body>
 
+
 </html>
+
+
+
+
+
